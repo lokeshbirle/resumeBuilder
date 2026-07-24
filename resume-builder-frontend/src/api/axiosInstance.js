3fea5075857695c8bdc113b2d697bc8e6f4ab8a8
Lokesh birle <lokeshbirle@niralnetworks.in>
2026-07-24 13:29:50 +0530

    updated code

--- resume-builder-frontend/src/api/axiosInstance.js
+++ resume-builder-frontend/src/api/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+    ? `${import.meta.env.VITE_API_BASE_URL}/api`
+    : "http://localhost:8080/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
